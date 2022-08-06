07a3a0e19aeae16d44d15797ece682fb1977afc2
Rishabh Kamthare <rkamthare98@gmail.com>
2022-08-06 18:26:18 +0530

    Updates

---
+++ precache-manifest.7d9f9a6312bc8b54dff2ec1570ef5a62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1edb6e3fee8954124978c60471a746",
+    "url": "/To-Do-List-App/index.html"
+  },
+  {
+    "revision": "b3f2758b9fa8d4c5e562",
+    "url": "/To-Do-List-App/static/js/2.f111bbf3.chunk.js"
+  },
+  {
+    "revision": "8b593973cafdf1847b1e",
+    "url": "/To-Do-List-App/static/js/main.ec57bf90.chunk.js"
+  },
+  {
+    "revision": "ccc2d159def393f15c15",
+    "url": "/To-Do-List-App/static/js/runtime-main.e87b0392.js"
+  }
+]);
